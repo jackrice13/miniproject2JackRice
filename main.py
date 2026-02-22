d683b3f9b9341a0d7470eda24a77810dd86b7849
jackrice13 <jackrice13@gmail.com>
2026-02-22 09:20:45 -0500

    Added folder creation and chart saving. Also updated .gitignore to ignore /charts/

--- main.py
+++ main.py
@@ -4,15 +4,15 @@
 
 # This project will be using Pandas dataframes. This isn't intended to be full blown data science project. The goal here is to come up with some question and then see what API or datasets you can use to get the information needed to answer that question. This will get you familar with working with datasets and asking questions, researching APIs and gathering datasets. If you get stuck here, please email me!
 #
-#     (5/5 points) Initial comments with your name, class and project at the top of your .py file.
-#     (5/5 points) Proper import of packages used.
+#     x(5/5 points) Initial comments with your name, class and project at the top of your .py file.
+#     x(5/5 points) Proper import of packages used.
 #     (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
 #     Think of some question you would like to solve such as:
 #     "How many homes in the US have access to 100Mbps Internet or more?"
 #     "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
 #     Here are some other great datasets: https://www.kaggle.com/datasets
-#     (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
-#     (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
+#     x(10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
+#     x(10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 #     (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 #     (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 #     (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
@@ -20,6 +20,12 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+#Creates /charts/ if it does not exsist
+chart = Path('charts')
+if not chart.exists():
+    Path('charts').mkdir()
 
 df = pd.read_csv('data/tech_employment_2000_2025.csv', index_col=0, low_memory=False)
 df = df.reset_index()
@@ -49,5 +55,9 @@ plt.title('AMD Layoffs and New Hires by Year')
 plt.xlabel('Year')
 plt.ylabel('Employment')
 plt.legend()
+plt.savefig(f'charts/AMD.png')
+plt.show()
+print(f'saving image to charts/AMD.png')
+
 plt.show()
 #push2222w
